fdb1107e3361d4a81507d9a6e9420820a705f026
savannahvaith <sav.vaithilingam@gmail.com>
2021-04-07 10:04:51 +0100

    SV: Added fruits example as recap

---
+++ src/com/qa/constructors/FruitRunner.java
@@ -0,0 +1,14 @@
+package com.qa.constructors;
+
+public class FruitRunner {
+	
+	public static void main(String[] args) {
+		
+		Fruits apple = new Fruits("Red");
+		Fruits appleG = new Fruits("Green");
+		Fruits orange = new Fruits("Orange");
+		Fruits peeledGrapes = new Fruits();
+		
+	}
+
+}
---
+++ src/com/qa/constructors/Fruits.java
@@ -0,0 +1,13 @@
+package com.qa.constructors;
+
+public class Fruits {
+	
+	private String colour; 
+	
+	public Fruits() {}
+	
+	public Fruits(String colour) {
+		this.colour = colour; 
+	}
+
+}
